b786600b19133ec5b3a2de406054f5639dfc9515
reodive <leoleo.sakai@icloud.com>
2025-03-11 03:27:31 +0900

    ver 1.3

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import CustomUser
 
-# Create your tests here.
+class UserTests(TestCase):
+    def test_signup(self):
+        response = self.client.post(reverse('signup'), {
+            'username': 'testuser',
+            'email': 'test@example.com',
+            'role': 'student',
+            'password1': 'ComplexPassword123',
+            'password2': 'ComplexPassword123'
+        })
+        self.assertEqual(response.status_code, 302)
