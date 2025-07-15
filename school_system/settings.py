98b34da8fec1cfa0172c2ef2d66144a25eecd2d4
reodive <leoleo.sakai@icloud.com>
2025-07-15 18:05:10 +0900

    ver3.31

--- school_system/settings.py
+++ school_system/settings.py
@@ -104,11 +104,7 @@ USE_TZ = True
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-
-# ✅ Django が `static/` を探せるようにする
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
